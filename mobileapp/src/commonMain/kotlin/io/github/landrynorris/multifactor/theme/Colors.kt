d40a4599f97da6f9858512954f5bdf4ef3c2b255
Landry Norris <landry.norris@live.com>
2022-08-22 19:01:52 -0500

    Added background color

--- mobileapp/src/commonMain/kotlin/io/github/landrynorris/multifactor/theme/Colors.kt
+++ mobileapp/src/commonMain/kotlin/io/github/landrynorris/multifactor/theme/Colors.kt
@@ -10,15 +10,20 @@ val IronGray = ColorPair(
     light = Color(red = 0xDA, green = 0xE0, blue = 0xE3),
     dark = Color(red = 0x3A, green = 0x3C, blue = 0x40))
 
+val Background = ColorPair(
+    light = Color(red = 0xCF, green = 0xCF, blue = 0xCF),
+    dark = Color(red = 0x84, green = 0x84, blue = 0x82)
+)
+
 val SecondaryGray = ColorPair(
     light = Color(red = 0xE3, green = 0xDD, blue = 0xDA),
     dark = Color(red = 0x40, green = 0x3C, blue = 0x3A))
 
 val lightColors = lightColors(primary = IronGray.light, secondary = SecondaryGray.light,
-    onPrimary = Color.Black, onSecondary = Color.Black)
+    onPrimary = Color.Black, onSecondary = Color.Black, background = Background.light)
 
 val darkColors = darkColors(primary = IronGray.dark, secondary = SecondaryGray.dark,
-    onPrimary = Color.White, onSecondary = Color.White)
+    onPrimary = Color.White, onSecondary = Color.White, background = Background.dark)
 
 val colors
     @Composable get() = if(isSystemInDarkTheme()) darkColors else lightColors
