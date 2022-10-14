61bb1bd75dbd3bf8da27ad83ee983a9e2b99622b
Landry Norris <landry.norris@live.com>
2022-10-14 13:30:40 -0500

    Create XCode iOS app

---
+++ iosAppXcode/iosAppXcode/iosAppXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppXcodeApp.swift
+//  iosAppXcode
+//
+//  Created by Landry Norris on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct iosAppXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
