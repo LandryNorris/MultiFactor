709fa585f736a6512e42e91bc9b9aca02113aec0
Landry Norris <landry.norris@live.com>
2023-01-10 19:30:54 -0600

    Add tests for totp

--- mobileapp/src/commonMain/kotlin/io/github/landrynorris/multifactor/components/AddOtpComponent.kt
+++ mobileapp/src/commonMain/kotlin/io/github/landrynorris/multifactor/components/AddOtpComponent.kt
@@ -45,4 +45,6 @@ class CreateOtpComponent(context: ComponentContext,
     }
 }
 
-data class CreateOtpState(val name: String = "", val secret: String = "", val type: OtpMethod = OtpMethod.HOTP)
+data class CreateOtpState(val name: String = "",
+                          val secret: String = "",
+                          val type: OtpMethod = OtpMethod.HOTP)
---
+++ otp/src/commonTest/kotlin/io/github/landrynorris/otp/test/TotpTest.kt
@@ -0,0 +1,37 @@
+package io.github.landrynorris.otp.test
+
+import io.github.landrynorris.otp.Totp
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TotpTest {
+    @Test
+    fun testTotpProgress() {
+        val secret = "abcdefg"
+        val totp = Totp(secret, "A name")
+        var time = 1234567L
+
+        totp.setTime(time)
+        assertEquals(0.23333333f, totp.progress)
+
+        time++
+        totp.setTime(time)
+        assertEquals(0.26666668f, totp.progress)
+
+        time++
+        totp.setTime(time)
+        assertEquals(0.3f, totp.progress)
+    }
+
+    @Test
+    fun testTotpCode8Digit() {
+        val secret = "abcdefg"
+        val totp = Totp(secret, "A name")
+
+        totp.setTime(123456789)
+        assertEquals("342241", totp.generatePin())
+
+        totp.setTime(234567890)
+        assertEquals("842292", totp.generatePin())
+    }
+}
