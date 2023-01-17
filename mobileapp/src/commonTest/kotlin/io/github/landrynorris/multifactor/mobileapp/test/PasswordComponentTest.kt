ef6a5e8e866aa2576aa4eb156434438a9f06baf8
Landry Norris <landry.norris@live.com>
2023-01-16 21:47:09 -0600

    Add test for toggling create password button

--- mobileapp/src/commonTest/kotlin/io/github/landrynorris/multifactor/mobileapp/test/PasswordComponentTest.kt
+++ mobileapp/src/commonTest/kotlin/io/github/landrynorris/multifactor/mobileapp/test/PasswordComponentTest.kt
@@ -5,14 +5,26 @@ import io.github.landrynorris.multifactor.components.PasswordComponent
 import io.github.landrynorris.multifactor.components.PasswordLogic
 import io.github.landrynorris.multifactor.repository.SettingsRepository
 import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class PasswordComponentTest {
 
     @Test
-    fun testTypePassword() {
+    fun testShowAndHideCreatePassword() {
         val component = createComponent()
 
-        component.createPasswordLogic
+        assertFalse(component.state.value.showAddPassword)
+        component.toggleAddPassword()
+        assertTrue(component.state.value.showAddPassword)
+        component.toggleAddPassword()
+        assertFalse(component.state.value.showAddPassword)
+        component.toggleAddPassword()
+        assertTrue(component.state.value.showAddPassword)
+        component.hideAddPassword()
+        assertFalse(component.state.value.showAddPassword)
+        component.hideAddPassword()
+        assertFalse(component.state.value.showAddPassword)
     }
 
     private fun createComponent(
