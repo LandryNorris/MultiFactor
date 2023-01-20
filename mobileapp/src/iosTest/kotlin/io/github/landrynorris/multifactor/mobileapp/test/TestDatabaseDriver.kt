fbe2f53d003c9b9df232f27a01af76f4cd00165d
Landry Norris <landry.norris@live.com>
2023-01-20 07:38:34 -0600

    cleanup warnings in test driver file

--- mobileapp/src/iosTest/kotlin/io/github/landrynorris/multifactor/mobileapp/test/TestDatabaseDriver.kt
+++ mobileapp/src/iosTest/kotlin/io/github/landrynorris/multifactor/mobileapp/test/TestDatabaseDriver.kt
@@ -1,12 +1,8 @@
 package io.github.landrynorris.multifactor.mobileapp.test
 
-import co.touchlab.sqliter.DatabaseConfiguration
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
-import com.squareup.sqldelight.drivers.native.wrapConnection
 import io.github.landrynorris.database.AppDatabase
-
-var index = 0
 
 actual fun createInMemoryTestDriver(): SqlDriver {
     return NativeSqliteDriver(AppDatabase.Schema, "test.db")
