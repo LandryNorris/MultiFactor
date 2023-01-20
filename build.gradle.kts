482b00ec91d44df237a19fc3591474e2c76880ac
Landry Norris <landry.norris@live.com>
2023-01-20 15:26:19 -0600

    Update SQLDelight version to get Kotlin 1.8.0 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
         classpath("org.jetbrains.compose:compose-gradle-plugin:$composeVersion")
         classpath("com.squareup.sqldelight:gradle-plugin:$sqlVersion")
         classpath("com.android.tools.build:gradle:7.3.1")
