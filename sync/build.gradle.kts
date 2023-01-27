b35e83e1c98a4316aef780e7fad9e4574db2f135
Landry Norris <landry.norris@live.com>
2023-01-27 09:34:20 -0600

    Add jvm support for sync

--- sync/build.gradle.kts
+++ sync/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 
 kotlin {
     android()
+    jvm()
 
     listOf(iosX64(), iosArm64(), iosSimulatorArm64())
 
@@ -32,7 +33,6 @@ kotlin {
             }
         }
         val androidMain by getting
-        val androidTest by getting
         val iosX64Main by getting
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
@@ -49,6 +49,12 @@ kotlin {
             dependsOn(commonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation("ch.qos.logback:logback-classic:1.4.5")
+            }
         }
     }
 }
