27e3eec9db8a287069be97fc871fb21abc63b7e8
Landry Norris <landry.norris@live.com>
2023-02-07 15:55:48 -0600

    Set up publishing for otp module

--- otp/build.gradle.kts
+++ otp/build.gradle.kts
@@ -5,7 +5,12 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlinx.kover")
     id("org.jetbrains.dokka")
+    id("maven-publish")
+    id("signing")
 }
+
+group = "io.github.landrynorris"
+version = "0.0.1"
 
 kotlin {
     android()
@@ -54,12 +59,65 @@ kotlin {
     }
 }
 
-android {
-    compileSdk = 32
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 32
+val properties by lazy {
+    Properties().also { it.load(project.rootProject.file("local.properties").inputStream()) }
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            artifact(javadocJar.get())
+            pom {
+                name.set("jni-utils")
+                description.set("Kotlin/Native bindings for JNI on android ndk")
+                url.set("https://github.com/LandryNorris/JniUtils")
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                scm {
+                    connection.set("https://github.com/LandryNorris/JniUtils.git")
+                    developerConnection.set("https://github.com/LandryNorris/JniUtils")
+                    url.set("https://github.com/LandryNorris/JniUtils")
+                }
+                developers {
+                    developer {
+                        id.set("landrynorris")
+                        name.set("Landry Norris")
+                        email.set("landry.norris0@gmail.com")
+                    }
+                }
+            }
+        }
     }
-    namespace = "io.github.landrynorris.otp"
+
+    repositories {
+        maven {
+            name = "sonatype"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+            credentials {
+                username = getProperty("sonatype.username")
+                password = getProperty("sonatype.password")
+            }
+        }
+    }
+}
+
+project.signing {
+    val secretKeyFile = getProperty("signing.secretKeyRingFile") ?: error("No key file found")
+    val secretKey = File(secretKeyFile).readText()
+    val signingPassword = getProperty("signing.password")
+    useInMemoryPgpKeys(secretKey, signingPassword)
+    sign(project.publishing.publications)
+}
+
+fun getProperty(name: String): String? {
+    return System.getProperty(name) ?: properties.getProperty(name)
 }
