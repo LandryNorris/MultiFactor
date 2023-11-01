8ee434e0f5f5d9b9540ff002469f0093254fc35c
Landry Norris <landry.norris@live.com>
2023-10-31 21:05:07 -0500

    use common dialog

--- mobileapp/src/commonMain/kotlin/io/github/landrynorris/multifactor/platform/Dialog.common.kt
+++
@@ -1,6 +0,0 @@
-package io.github.landrynorris.multifactor.platform
-
-import androidx.compose.runtime.Composable
-
-@Composable
-internal expect fun Dialog(onDismissRequest: () -> Unit, content: @Composable () -> Unit)
