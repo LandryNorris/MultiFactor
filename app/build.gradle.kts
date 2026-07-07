113e03f560d9459df4b6cc41e3f581f4a00344b5
LandryNorris <37489471+LandryNorris@users.noreply.github.com>
2026-07-06 21:06:07 -0500

    Drop iosX64 support to prepare for compose update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,6 @@ kotlin {
     jvm()
 
     listOf(
-        iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     )
--- password-generator/build.gradle.kts
+++ password-generator/build.gradle.kts
@@ -19,7 +19,7 @@ kotlin {
     }
     jvm()
 
-    listOf(iosX64(), iosArm64(), iosSimulatorArm64())
+    listOf(iosArm64(), iosSimulatorArm64())
 
     sourceSets {
         commonTest.dependencies {
