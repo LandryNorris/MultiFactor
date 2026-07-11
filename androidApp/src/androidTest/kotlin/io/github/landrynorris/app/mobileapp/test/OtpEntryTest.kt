0d64212f1323df1b7692ccae9fe5fe627f87b0f7
LandryNorris <37489471+LandryNorris@users.noreply.github.com>
2026-07-11 17:45:24 -0500

    Fix failing tests

--- androidApp/src/androidTest/kotlin/io/github/landrynorris/app/mobileapp/test/OtpEntryTest.kt
+++ androidApp/src/androidTest/kotlin/io/github/landrynorris/app/mobileapp/test/OtpEntryTest.kt
@@ -2,7 +2,7 @@ package io.github.landrynorris.app.mobileapp.test
 
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.ComposeTestRule
-import androidx.compose.ui.test.junit4.createEmptyComposeRule
+import androidx.compose.ui.test.junit4.v2.createEmptyComposeRule
 import io.github.landrynorris.otp.Hotp
 import io.github.landrynorris.otp.Totp
 import org.junit.Rule
@@ -73,6 +73,8 @@ class OtpEntryTest {
 
             newEntry.assertExists("No new entry added")
             newEntry.performTouchInput { swipeLeft() }
+            composeRule.mainClock.advanceTimeBy(500)
+            awaitIdle()
 
             it.withOtpLogic {
                 val anyMatch = state.value.otpList.any { otp ->
@@ -122,6 +124,8 @@ class OtpEntryTest {
 
             newEntry.assertExists("No new entry added")
             newEntry.performTouchInput { swipeLeft() }
+            composeRule.mainClock.advanceTimeBy(500)
+            awaitIdle()
 
             it.withOtpLogic {
                 val anyMatch = state.value.otpList.any { otp ->
